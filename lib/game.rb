8173eef2ffc5e5031633df129576429918dd94cc
Deanna Vazquez <deanna.vazquez@kin.com>
2025-09-23 11:45:01 -0500

    Add play loop with alternating turns, add move validation for empty spaces and bounds, Implement winner? checks for rows, columns, diagonals

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ class Game
     welcome_message
     initialize_players
     @board = create_board
-    # @current_player_index = 0
+    @current_player_index = 0
   end
 
   def initialize_players
@@ -20,13 +20,68 @@ class Game
   end
 
   def print_board
-    puts ' 0   1   2'
+    puts '  0   1   2'
     sep = ' ---+---+---'
     @board.each_with_index do |row, r|
       print r
       puts " #{row[0]} | #{row[1]} | #{row[2]} "
       puts sep unless r == 2
     end
+  end
+
+  def current_player
+    [@player1, @player2][current_player_index]
+  end
+
+  def switch_turn
+    @current_player_index = (@current_player_index + 1) % [@player1, @player2].size
+  end
+
+  def valid_move?(row, col)
+    (0..2).include?(row) && (0..2).include?(col) && @board[row][col] == ' '
+  end
+
+  def play
+    loop do
+      puts "Player #{current_player.number} (#{current_player.symble}) enter your move (row col): "
+      input = gets.chomp
+      row, col = input.split.map(&:to_i)
+      if valid_move?(row, col)
+        @board[row][col] = current_player.symbol
+        print_board
+        switch_turn
+      else
+        puts 'Invalid move. Try again.'
+        next # restart loop without switching turns
+      end
+    end
+  end
+
+  def winner?
+    # Rows
+    @board.each do |row|
+      first = row.first
+      return first if row.uniq.size == 1 && first != ' '
+    end
+
+    # Column
+    (0..2).each do |col|
+      column = [@board[0][col], @board[1][col], @board[2][col]]
+      first = column.first
+      return first if column.uniq.size == 1 && first != ' '
+    end
+    # Diagonals
+    diagonal1 = [@board[0][0], @board[1][1], @board[2][2]]
+    return diagonal1.first if diagonal1.uniq.size == 1 && diagonal1.first != ' '
+
+    diagonal2 = [@board[0][2], @board[1][1], @board[2][0]]
+    return diagonal2.first if diagonal2.uniq.size == 1 && diagonal2.first != ' '
+
+    nil
+  end
+
+  def draw?
+    @board.flatten.none? { |cell| cell == ' ' } && winner?.nil?
   end
 
   def welcome_message # rubocop:disable Metrics/MethodLength
